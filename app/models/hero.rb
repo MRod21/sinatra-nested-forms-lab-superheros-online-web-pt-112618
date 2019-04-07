3a23cd2f223c9d7c77666a674e09b8b9ef514b2e
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2019-04-07 04:29:49 +0000

    Automatically backed up by Learn

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,18 +1,18 @@
 class SuperHero
   attr_accessor :name, :power, :bio
 
-   def self.all
+  def self.all
     @@all ||= []
   end
 
-   def initialize(opts={})
+  def initialize(opts={})
     @name  = opts[:name]
     @power = opts[:power]
     @bio   = opts[:bio]
     self.save
   end
 
-   def save
+  def save
     self.class.all << self
   end
 end
